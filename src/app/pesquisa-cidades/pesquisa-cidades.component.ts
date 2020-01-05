c5fdb2499528b25cb150a1626ce1a7d1bba714c2
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-01-05 20:45:34 -0300

    Criando o componente pesquisa de cidades

--- src/app/pesquisa-cidades/pesquisa-cidades.component.ts
+++ src/app/pesquisa-cidades/pesquisa-cidades.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PesquisaCidadesComponent implements OnInit {
 
+  cidades = [
+    { id:1, nome:'Fortaleza', estado:'Ceará' },
+    { id:2, nome:'Quixadá', estado:'Ceará' },
+    { id:3, nome:'Recife', estado:'Pernambuco' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
